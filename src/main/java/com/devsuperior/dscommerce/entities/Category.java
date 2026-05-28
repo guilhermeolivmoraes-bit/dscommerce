8f0007d427aca4bb9eb56ca7df85a51478352954
guilhermeolivmoraes-bit <guilherme.olivmoraes@gmail.com>
2026-05-28 09:57:57 -0300

    OrderItemPk and OrderItem - 2

--- src/main/java/com/devsuperior/dscommerce/entities/Category.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Category.java
@@ -1,4 +1,48 @@
 package com.devsuperior.dscommerce.entities;
 
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_category")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    private String Name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        Id = id;
+        Name = name;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
+    }
 }
