ccf59ca6f629658fb04222ae24cdddf8f60d2754
guilhermeolivmoraes-bit <guilherme.olivmoraes@gmail.com>
2026-08-11 10:15:41 -0300

    Fix validation

--- src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -8,6 +8,7 @@ import com.devsuperior.dscommerce.entities.Product;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
@@ -22,6 +23,7 @@ public class ProductDTO {
     @Size(min = 10, message = "descrição precisa ter no mínimo 10 caracteres")
     @NotBlank(message = "Campo Requerido")
     private String description;
+    @NotNull(message = "Campo Requerido")
     @Positive (message = "o preço deve ser positivo")
     private Double price;
     private String imgUrl;
